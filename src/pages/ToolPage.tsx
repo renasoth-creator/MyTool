23e1514a170ff93d4a2bbfe64f01965bb9bd7b36
renasoth-creator <renasoth@gmail.com>
2025-11-30 15:41:45 +0300

    .

--- src/pages/ToolPage.tsx
+++ src/pages/ToolPage.tsx
@@ -59,7 +59,7 @@ const ToolPage: React.FC<ToolPageProps> = ({ toolId }) => {
     <Layout>
       {/* BREADCRUMB */}
       <div className="mb-6 flex items-center gap-2 text-xs text-slate-600">
-        <Link to="/" className="hover:text-[#ff7a1a] hover:underline">
+        <Link to="/" className="hover:text-[#ffffff] hover:underline">
           All tools
         </Link>
         <span>/</span>
