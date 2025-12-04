5a62e1d2d4ed4174f93819b22407b88ec6a42603
renasoth-creator <renasoth@gmail.com>
2025-12-04 12:38:41 +0300

    Las

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -22,7 +22,7 @@ export default function Signup() {
     setStatus("sending");
 
     try {
-      const res = await fetch(`${BACKEND_URL}/auth/signup`, {
+      const res = await fetch(`${BACKEND_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
