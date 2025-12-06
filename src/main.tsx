b183fa68b03b04396eaff14fa69b12f6c6796c74
naxus <renasoth@gmail.com>
2025-12-06 19:39:53 +0300

    more tools

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthContext";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
