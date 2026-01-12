bea4dcefd0e213f90c19bd4300af601b92addaac
naxus <renasoth@gmail.com>
2026-01-13 00:09:36 +0300

    Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 
 // VERY IMPORTANT: Tailwind + global styles must be imported
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
